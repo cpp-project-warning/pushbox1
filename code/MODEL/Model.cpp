51dfe04026c96439de4e0e17ba420c948e01bebd
Wsh <138436351+Wsh1273591351@users.noreply.github.com>
2023-07-12 18:12:50 +0800

    Add files via upload

--- code/MODEL/Model.cpp
+++ code/MODEL/Model.cpp
@@ -1,4 +1,5 @@
-#include "MODEL.h"
+#include "Model.h"
+#include "get.h"
 
 // Model::Model(int box_number, int wall[MAXN][MAXN], position destination[], position box[], position player) :
 // 	game_map(box_number, wall, destination), p(player), p_init(player)
@@ -10,12 +11,15 @@
 // 	}
 // 	all_box_init = all_box;
 // }
-//extern int get_box_number();
-//extern int** get_wall();
-//extern position* get_destination(int box_number);
 
-//extern position* get_box(int box_number);
-//extern position get_player();
+extern int current_map_count;
+
+extern int get_box_number();
+extern int** get_wall();
+extern position* get_destination(int box_number);
+extern position* get_box(int box_number);
+extern position get_player();
+
 Model::Model()
 {
 
@@ -26,29 +30,29 @@ Model::~Model()
 
 }
 
-Map Model::get_game_map()
+Map& Model::get_game_map()
 {
 	return game_map;
 }
 
-Player Model::get_player()
+Player& Model::get_player()
 {
 	return p;
 }
 
-std::set<Box> Model::get_all_box()
+std::set<Box>& Model::get_all_box()
 {
 	return all_box;
 }
 
 void Model::set_model()
 {
+	count_step = 0;
 	int box_number = get_box_number();
-	int wall[MAXN][MAXN];
-	get_wall(wall);
+	int** wall = get_wall();
 	position *destination = get_destination(box_number);
 	position *box = get_box(box_number);
-	position player = ::get_player();
+	position player = get_player().get_position();
 	game_map.set_box_number(box_number);
 	game_map.set_wall(wall);
 	std::set<position> des;
@@ -70,13 +74,14 @@ void Model::set_model()
 void Model::reset_model()
 {
 	p.set_position(p_init.get_position());
+	p.set_direction(Down);
 	all_box = all_box_init;
 }
 
-bool Model::move_operator(char c)
+bool Model::move_operation(direction dir)
 {
-	p.move_player(c);
-	return true;
+	p.set_direction(dir);
+	return p.move_player(dir, all_box);
 }
 
 bool Model::judge_if_win()
@@ -94,4 +99,30 @@ bool Model::judge_if_win()
 	{
 		return false;
 	}
+}
+
+void Model::change_current_game(int operation)
+{
+	if(operation == -1)
+	{
+		if(current_map_count != 1)
+		{
+			current_map_count -= 1;
+			set_model();
+		}
+	}
+	else if(operation == 0)
+	{
+		reset_model();
+	}
+	else if(operation == 1)
+	{
+		current_map_count += 1;
+		set_model();
+	}
+}
+
+int& Model::get_count_step()
+{
+	return count_step;
 }
