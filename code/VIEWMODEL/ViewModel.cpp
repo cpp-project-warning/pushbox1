4fad29af58b2dad051d14673fb2d22386b17a7e6
Wsh <138436351+Wsh1273591351@users.noreply.github.com>
2023-07-13 15:08:47 +0800

    Add files via upload

--- code/VIEWMODEL/ViewModel.cpp
+++ code/VIEWMODEL/ViewModel.cpp
@@ -1,6 +1,9 @@
 #include "ViewModel.h"
 
-ViewModel::ViewModel()
+extern int level;
+
+ViewModel::ViewModel():m_move(std::make_shared<MoveCommand>),
+m_sink(std::make_shared<ViewModelSink>(this))
 {
 
 }
@@ -13,6 +16,78 @@ ViewModel::~ViewModel()
 void ViewModel::set_model(const std::shared_ptr<Model>& model)
 {
 	m_model = model;
+}
+
+void ViewModel::set_direction(direction dir)
+{
+	m_model->get_player().set_direction(dir);
+}
+
+void ViewModel::set_change(int operation)
+{
+	m_model->change_current_game(operation);
+}
+
+	// Player ViewModel::set_player();
+	// std::set<Box> ViewModel::set_all_box();
+	// void ViewModel::set_wall(int w[][MAXN]);
+	// std::set<position> sViewModel::et_destination();
+
+void ViewModel::get_ViewMap(int viewMap[][MAXN])
+{
+	for (int i = 0; i < MAXN; i++)
+	{
+		for (int j = 0; j < MAXN; j++)
+		{
+			viewMap[i][j] = 0;
+		}
+	}
+	int wall[MAXN][MAXN];
+	get_wall(wall);
+	for (int i = 0; i < MAXN; i++)
+	{
+		for (int j = 0; j < MAXN; j++)
+		{
+			if (wall[i][j] == 1)
+			{
+				viewMap[i][j] = 1;
+			}
+		}
+	}
+	Player p = get_player();
+	position p_position = p.get_position();
+	viewMap[p_position.x][p_position.y] = 4;
+	std::set<Box> box = get_all_box();
+	for (auto i = box.begin(); i != box.end(); i++)
+	{
+		position box_positon=(*i).get_position();
+		viewMap[box_positon.x][box_positon.y] = 3;
+	}
+	std::set<position> des= get_destination();
+	for (auto i = des.begin(); i != des.end(); i++)
+	{
+		viewMap[(*i).x][(*i).y] = 2;
+	}
+}
+
+int ViewModel::get_Level()
+{
+	return level;
+}
+
+int ViewModel::get_Step()
+{
+	return m_model->get_count_step();
+}
+
+bool ViewModel::get_Ifsuccess()
+{
+	return m_model->judge_if_win();
+}
+
+direction ViewModel::get_Orientation()
+{
+	return m_model->get_player().get_direction();
 }
 
 Player ViewModel::get_player()
@@ -42,16 +117,25 @@ std::set<position> ViewModel::get_destination()
 
 bool ViewModel::move_operation(direction dir)
 {
-	return true;	
-}
-	
-bool ViewModel::if_next_game()
-{
-	return true;
+	return m_model->move_operation(dir);
 }
 
-bool ViewModel::if_reset()
+void ViewModel::skip_operation(int operation)
 {
-	return true;
+	m_model->change_current_game(operation);
 }
 
+std::shared_ptr<ICommandBase> ViewModel::get_MoveCommand() noexcept
+{
+	return std::static_pointer_cast<ICommandBase>(m_move);
+}
+
+std::shared_ptr<ICommandBase> ViewModel::get_SkipCommand() noexcept
+{
+	return std::static_pointer_cast<ICommandBase>(m_skip);
+}
+
+std::shared_ptr<IPropertyNotification> ViewModel::get_PropertySink() noexcept
+{
+	return std::static_pointer_cast<IPropertyNotification>(m_sink);
+}
--- code/VIEWMODEL/ViewModel.h
+++ code/VIEWMODEL/ViewModel.h
@@ -4,24 +4,49 @@
 
 #include "../MODEL/Model.h"
 #include "../COMMON/COMMON.h"
+#include "commands/MoveCommand.h"
+#include "commands/SkipCommand.h"
+#include "sinks/ViewModelSink.h"
 
-class ViewModel
+class ViewModel:public Proxy_PropertyNotification<ViewModel>,
+	public Proxy_CommandNotification<ViewModel>
 {
 private:
 	std::shared_ptr<Model> m_model;
+	std::shared_ptr<MoveCommand> m_move;
+	std::shared_ptr<SkipCommand> m_skip;
+	std::shared_ptr<ViewModelSink> m_sink;
 
 public:
 	ViewModel();
 	~ViewModel();
+
 	void set_model(const std::shared_ptr<Model>& model);
+
+	void set_direction(direction dir);
+	void set_change(int operation);
+
+	//properties
+	void get_ViewMap(int viewMap[][MAXN]);
 	Player get_player();
 	std::set<Box> get_all_box();
-	//int** get_wall();
 	void get_wall(int w[][MAXN]);
 	std::set<position> get_destination();
+
+	int get_Level();
+	int get_Step();
+	bool get_Ifsuccess();
+	direction get_Orientation();
+
 	bool move_operation(direction dir);
-	bool if_next_game();
-	bool if_reset();
+	void skip_operation(int operation);
+
+	//commands
+	std::shared_ptr<ICommandBase> get_MoveCommand() noexcept;
+	std::shared_ptr<ICommandBase> get_SkipCommand() noexcept;
+
+	//notifications
+	std::shared_ptr<IPropertyNotification> get_PropertySink() noexcept;
 };
 
 #endif
