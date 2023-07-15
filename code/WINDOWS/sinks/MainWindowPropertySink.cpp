e43432b7441d1fcfbecd9ec15ed2f183136eb3fa
enninnvxi <106723763+enninnvxi@users.noreply.github.com>
2023-07-15 11:05:53 +0800

    Add files via upload

--- code/WINDOWS/sinks/MainWindowPropertySink.cpp
+++ code/WINDOWS/sinks/MainWindowPropertySink.cpp
@@ -14,9 +14,17 @@ MainWindowPropertySink::MainWindowPropertySink(MainWindow* pW) throw() : m_pW(pW
 
 void MainWindowPropertySink::OnPropertyChanged(const std::string& str)
 {
+//	m_pW->draw();
+#ifndef NDEBUG
+	qDebug() << "windows_property_sink::on property changed " << str << "\n"<<"level:"<<level<<"step: \n";
+#endif // !NDEBUG
 	if( str == "doSuccess" ) {
 		m_pW->popsuccess();
 	}
+	else {
+		m_pW->update();
+	}
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////
